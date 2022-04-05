d9d7857c546b14866ad82b0c9d41b8ac850e09be
ztashneem <z.tashneem12@gmail.com>
2022-04-05 15:52:09 +0600

    coustom hooks add

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { ReviewContext } from '../../App';
+import CustomLink from '../CoustomLink/CustomLink';
+import HomeReview from '../HomePageReview/HomeReview';
+
 
 const Home = () => {
+    const [review, setReview] = useContext(ReviewContext);
+    const newReview = [...review];
+    console.log(newReview.slice(0, 3));
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setReview(data))
+    }, [setReview])
     return (
         <div>
-            
+            <div className="row sills pt-lg-5">
+                <div className="col-md-7 ">
+                    <img src={`https://cdn.pixabay.com/photo/2019/02/26/02/13/ukulele-4021002_1280.jpg`} className='img-fluid ' alt="" />
+                </div>
+                <div className="col-md-4 py-3 ">
+                    <h1>Model Y Illuminated Car Sills</h1>
+                    <h1 className='text-muted'>Installation Manual</h1>
+                    <p>
+                    Shopping for the right musical instruments can be a challenging task which is why out expert musicians put together the best buying guides for the most popular best selling types of musical instruments on the market today.
+                    </p>
+
+                    <p>
+                        <CustomLink className='btn btn-outline-dark px-lg-5 px-3' to='/blogs'> Live Demo</CustomLink>
+                    </p>
+                </div>
+                <h1 className='text-center my-5 specing'>Our Happy Clients Says </h1>
+                <div className="review-container">
+
+                    {
+                        newReview.slice(0, 3).map(singleReview => <HomeReview key={singleReview.id} singleReview={singleReview}></HomeReview>)
+                    }
+
+                </div>
+
+                <p className='my-5 text-center'><Link className='btn px-lg-5 px-3 btn-outline-dark' to='/reviews'>See All Reviews</Link></p>
+            </div>
+
         </div>
     );
 };
---
+++ src/Components/SingleReview/SingleREview.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import { BsStarHalf, BsStarFill } from 'react-icons/bs';
+
+const SingleReview = ({ review }) => {
+    const { name, imageURL, subtittle, ratings, reviewperson } = review;
+    return (
+        <div className=' container mb-3 '>
+            <div className=" text-center">
+
+                <Card style={{ width: '20rem', height: '35rem' }} >
+                    <Card.Body>
+                        <Card.Img className='object-cover' variant="top" src={imageURL} height={'260px'} />
+                        <Card.Title className='my-3'>{name}</Card.Title>
+                        <Card.Subtitle className="mb-3 text-muted">{subtittle}</Card.Subtitle>
+                        <Card.Text>
+                            {reviewperson.slice(0, 170) + '...'}
+                        </Card.Text>
+                        <Card.Link to='/'>{ratings !== 5 ?
+                            <p className=''>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarHalf className='textstart'></BsStarHalf>
+                                <span className='text-muted ms1-2'> <small>({ratings})</small></span>
+                            </p>
+                            :
+                            <p>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className='me-2 textstart'></BsStarFill>
+                                <BsStarFill className=' textstart'></BsStarFill>
+                                <span className='text-muted ms1-2'> <small>({ratings})</small></span>
+                            </p>
+                        }  </Card.Link>
+
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default SingleReview;
